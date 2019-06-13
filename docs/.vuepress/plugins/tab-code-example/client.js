b6342c05f47baf0c832a7969cd0c28d8a1e4913e
Goren G <yong.gu@qlink.mobi>
2019-06-13 20:57:36 +0800

    fix: docs:build failed
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- docs/.vuepress/plugins/tab-code-example/client.js
+++ docs/.vuepress/plugins/tab-code-example/client.js
@@ -2,7 +2,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faPlay, faUndo } from '@fortawesome/free-solid-svg-icons'
-import 'whatwg-fetch'
 
 import Demo from './components/Demo'
 
